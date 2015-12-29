0d77c72a0f427b62e24fc68891f630fc0e3840f6
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2015-12-28 23:53:10 -0800

    Trying some quick CI

---
+++ main.d
@@ -0,0 +1,32 @@
+module main;
+
+import simulated;
+
+import core.thread, core.time;
+
+import std.stdio;
+
+
+void main()
+{
+
+
+	Mouse.moveTo(100,200);
+
+	Mouse.buttonClick(Mouse.Buttons.Left);
+
+
+	Keyboard.keyPress(Keyboard.Keys.LShift);
+	Keyboard.keyType(Keyboard.Keys.A);
+	Keyboard.keyRelease(Keyboard.Keys.LShift);
+	Keyboard.keyType(Keyboard.Keys.B);
+	Thread.sleep(dur!("msecs")(115));
+	
+
+
+}
+
+
+
+
+
